8d012f93486fcbe2b951e0324dd6ec940db4233b
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2024-04-06 12:11:55 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,7 @@ const connection = require('./connection.js')
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors(
-    {
-        origin:["https://whatsapp-clone-frontend-tau.vercel.app/"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
+app.use(cors());
 app.use('/', router)
 const port = 3001;
 
